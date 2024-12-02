89f583c7641fea79be6b8a5778345bc812413031
abdulazizmatrok <kingg767676@gmail.com>
2024-12-02 18:55:51 +0300

    Implemented multiplication logic and added unit tests

---
+++ CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.calculator;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    
+       @Test
+ 
+      public void testMultiply() {
+        System.out.println("multiply");
+        double num1 = 5.0;
+        double num2 = 4.0;
+        Calculator.multiply(num1, num2);
+   
+    }
+    
+    
+    
+    
+}
